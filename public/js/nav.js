ebba7ea4379bcaceec387a3afade954b1ee4ca64
vinevv <viniciusvasconcellosvieira@gmail.com>
2023-04-25 19:53:31 -0300

    fix: acesso à página de login

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,7 +1,7 @@
 const createNav = () => {
-    let nav = document.querySelector('.navbar');
+  let nav = document.querySelector(".navbar");
 
-    nav.innerHTML = `
+  nav.innerHTML = `
         <div class="nav">
             <img src="img/dark-logo.png" class="brand-logo" alt="">
             <div class="nav-items">
@@ -27,36 +27,36 @@ const createNav = () => {
             <li class="link-item"><a href="#" class="link">accessories</a></li>
         </ul>
     `;
-}
+};
 
 createNav();
 
 // nav popup
-const userImageButton = document.querySelector('#user-img');
-const userPopup = document.querySelector('.login-logout-popup');
-const popuptext = document.querySelector('.account-info');
-const actionBtn = document.querySelector('#user-btn');
+const userImageButton = document.querySelector("#user-img");
+const userPopup = document.querySelector(".login-logout-popup");
+const popuptext = document.querySelector(".account-info");
+const actionBtn = document.querySelector("#user-btn");
 
-userImageButton.addEventListener('click', () => {
-    userPopup.classList.toggle('hide');
-})
+userImageButton.addEventListener("click", () => {
+  userPopup.classList.toggle("hide");
+});
 
 window.onload = () => {
-    let user = JSON.parse(sessionStorage.user || null);
-    if(user != null){
-        // means user is logged in
-        popuptext.innerHTML = `log in as, ${user.name}`;
-        actionBtn.innerHTML = 'log out';
-        actionBtn.addEventListener('click', () => {
-            sessionStorage.clear();
-            location.reload();
-        })
-    } else{
-        //user is logged out
-        popuptext.innerHTML = 'log in to place order';
-        actionBtn.innerHTML = 'log in';
-        actionBtn.addEventListener('click', () => {
-            location.href = '/login';
-        })
-    }
-}
+  let user = JSON.parse(sessionStorage.user || null);
+  if (user != null) {
+    // means user is logged in
+    popuptext.innerHTML = `log in as, ${user.name}`;
+    actionBtn.innerHTML = "log out";
+    actionBtn.addEventListener("click", () => {
+      sessionStorage.clear();
+      location.reload();
+    });
+  } else {
+    //user is logged out
+    popuptext.innerHTML = "log in to place order";
+    actionBtn.innerHTML = "log in";
+    actionBtn.addEventListener("click", () => {
+      location.href = "/login.html";
+    });
+  }
+};
